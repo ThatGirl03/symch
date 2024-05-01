5cbead13dbc868137c1d5a40fa27b6aa1c934acb
22238460 <22238460@dut4life.ac.za>
2024-05-01 20:36:46 +0200

    Second commit

--- app/src/main/java/com/example/symch/symptom_checker.java
+++ app/src/main/java/com/example/symch/symptom_checker.java
@@ -3,6 +3,7 @@ package com.example.symch;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class symptom_checker extends AppCompatActivity {
@@ -15,7 +16,25 @@ public class symptom_checker extends AppCompatActivity {
 
         WebView webView = findViewById(R.id.webview);
         webView.getSettings().setJavaScriptEnabled(true);  // Enable JavaScript.
-        webView.loadUrl("https://www.medifind.com/symptom-checker");  // Load the URL of your choice.
-    }
 
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // Inject CSS to change the background color and hide specific elements
+                String css = "body { background-color: #800080 !important; }" +
+                        "div.header, .site-name, .hamburger-menu { display: none !important; }";
+                String javascript = "javascript:(function() {" +
+                        "var style = document.createElement('style');" +
+                        "style.type = 'text/css';" +
+                        "style.appendChild(document.createTextNode('" + css + "'));" +
+                        "document.head.appendChild(style);" +
+                        "})()";
+                webView.evaluateJavascript(javascript, null);
+            }
+        });
+
+        // Load the URL after setting up the client
+        webView.loadUrl("https://www.symptoma.co.za/");
+    }
 }
